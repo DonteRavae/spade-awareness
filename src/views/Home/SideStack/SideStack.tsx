f75f560d63f8ad30a6e6ecdcc16cb27481acebde
Donte Ravae <donteravae@gmail.com>
2022-07-08 02:31:11 -0400

    convert SideStack icons to anchors

--- src/views/Home/SideStack/SideStack.tsx
+++ src/views/Home/SideStack/SideStack.tsx
@@ -3,21 +3,34 @@ import Icon from "../../../components/Icons/Icons";
 
 const SideStackContainer = styled.section`
   width: 10%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
   transition: all 300ms ease-in-out;
+  z-index: 99;
+  padding: 50px 0;
 `;
 
 const SideStack = () => {
   return (
     <SideStackContainer>
-      <Icon icon="spade_s" height={70} width={55} />
-      <Icon icon="spade_p" height={70} width={55} />
-      <Icon icon="spade_a" height={70} width={55} />
-      <Icon icon="spade_d" height={70} width={55} />
-      <Icon icon="spade_e" height={70} width={55} />
+      <a href="#suicide">
+        <Icon icon="spade_s" height={70} width={55} />
+      </a>
+      <a href="#ptsd">
+        <Icon icon="spade_p" height={70} width={55} />
+      </a>
+      <a href="#anxiety">
+        <Icon icon="spade_a" height={70} width={55} />
+      </a>
+      <a href="#depression">
+        <Icon icon="spade_d" height={70} width={55} />
+      </a>
+      <a href="#epilepsy">
+        <Icon icon="spade_e" height={70} width={55} />
+      </a>
     </SideStackContainer>
   );
 };
